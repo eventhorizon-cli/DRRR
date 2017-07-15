979c2430e35202927e51c93fb9ddebcb359a2a41
黄凯华 <blurhkh@hotmail.com>
2017-07-15 19:49:42 +0800

    修改Dto

---
+++ DRRR.Server/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DRRR.Server.Dtos
+{
+    public class UserDto
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
