396694b78ae14fa6f74705aeb470639342cd5224
黄凯华 <blurhkh@hotmail.com>
2017-07-15 19:44:16 +0800

    修改DTO命名

---
+++ DRRR.Server/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DRRR.Server.DTOs
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
