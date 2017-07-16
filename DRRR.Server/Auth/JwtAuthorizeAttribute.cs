2352c4159765f6ee72f85d9f2c6af70f6702aa1d
黄凯华 <blurhkh@hotmail.com>
2017-07-16 20:20:25 +0800

    精简JwtAuthorizeAttribute代码

--- DRRR.Server/Auth/JwtAuthorizeAttribute.cs
+++ DRRR.Server/Auth/JwtAuthorizeAttribute.cs
@@ -12,26 +12,13 @@ namespace DRRR.Server.Auth
     public class JwtAuthorizeAttribute : AuthorizeAttribute
     {
         /// <summary>
-        /// 有权访问该资源的用户角色数组
-        /// </summary>
-        public new Roles[] Roles { get; set; }
-
-        /// <summary>
         /// 对指定资源访问进行JWT认证
         /// </summary>
-        public JwtAuthorizeAttribute() : base("Jwt")
+        /// <param name="roles">有权访问该资源的用户角色数组</param>
+        public JwtAuthorizeAttribute(params Roles[] roles) : base("Jwt")
         {
-            if (Roles == null)
-            {
-                // 默认对所有通过JWT认证的用户开放
-                Roles = new Roles[]
-                {
-                    Auth.Roles.Guest,
-                    Auth.Roles.User,
-                    Auth.Roles.Admin
-                };
-            }
-
+            // 默认对所有通过JWT认证的用户开放
+            roles = roles ?? new[] { Auth.Roles.Guest, Auth.Roles.User, Auth.Roles.Admin };
             // 通过逗号分隔
             base.Roles = string.Join(",", Roles);
         }
