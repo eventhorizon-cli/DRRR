f5a806bc074e0c0328384d9cec432701cd656620
黄凯华 <blurhkh@hotmail.com>
2017-07-30 22:21:07 +0800

    完成导航栏css调整

--- DRRR.Client/src/app/app.component.ts
+++ DRRR.Client/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  /**
+   * 选中菜单选项以滑动方式隐藏菜单
+   * @param {string} expanded 表示菜单是否属于展开状态
+   * @param {HTMLInputElement} el 触发菜单展开和关闭的按钮
+   */
+  slideUp(expanded: string, el: HTMLInputElement) {
+    if (expanded === 'true') {
+      el.click();
+    }
+  }
 }
