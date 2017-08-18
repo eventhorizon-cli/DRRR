04a38f650344d12c61993467b37b7887f2a9da83
黄凯华 <blurhkh@hotmail.com>
2017-08-18 22:50:26 +0800

    升级至2.0正式版

--- DRRR.Server/Models/ChatRoom.cs
+++ DRRR.Server/Models/ChatRoom.cs
@@ -10,6 +10,7 @@ namespace DRRR.Server.Models
         public int CurrentUsers { get; set; }
         public bool? IsEncrypted { get; set; }
         public bool? IsPermanent { get; set; }
+        public bool? IsHidden { get; set; }
         public int MaxUsers { get; set; }
         public string Name { get; set; }
         public int OwnerId { get; set; }
